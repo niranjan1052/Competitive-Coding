a4afc0fc8803aff13a044557d009229012b02c4c
Sid1054 <niranjan3611@gmail.com>
2017-02-11 11:54:40 -0600

    Search Insert position

---
+++ LeetCode_35.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        int mid;
+        while(start<=end){
+            mid = start+(end-start)/2;
+            if(nums[mid]==target)
+               { return mid;}
+            if(nums[mid]>target)
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+       return start;     
+    }
+}
